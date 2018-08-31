922f91d5ff93382398e620e3621e1819f0bb0b8b
Liam <lgaskell@freewebstore.com>
2018-08-31 10:35:28 +0100

    Replaced example file with correct file.

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# Node-CallOfDuty
-Node Wrapper for the "private" Call Of Duty API.
+# Call Of Duty API Wrapper
+
+Call of Duty Api is a wrapper for the "private" API that Activision use on the callofduty.com website.
+
+# Initialize Module
+```javascript
+const API = require('call-of-duty-api');
+```
+
+# List of Platforms
+- psn
+- steam
+- xbl
+
+# Get Stats
+```javascript
+    API.IWStats(<gamertag>, <platform>).then((output) => {
+      console.log(output);  
+    }).catch((err) => {
+        console.log(err);
+    });
+```
+
+# Output
+```javascript 
+{
+    title: 'iw',
+    platform: 'platform',
+    username: 'gamertag',
+    mp:
+    { lifetime: { all: [Object], mode: [Object] },
+    weekly: null,
+    level: 0,
+    maxLevel: 0,
+    levelXpRemainder: 0,
+    levelXpGained: 0,
+    prestige: 0,
+    prestigeId: 0,
+    maxPrestige: 0 },
+    zombies:
+    { lifetime: { all: [Object], mode: {} },
+    weekly: { all: [Object], mode: {} } },
+    engagement: { timePlayedAll: 440544, seasonPass: 1 } 
+}
+```
--- index.js
+++ index.js
@@ -1,7 +1,94 @@
-const API = require('call-of-duty-api');
+const request = require('request');
+const util = require('util');
 
-API.IWStats("lierrmm", "psn").then((output) => {
-  console.log(output);  
-}).catch((err) => {
-    console.log(err);
-});
+const defaultBaseURL = "https://my.callofduty.com/api/papi-client/";
+const userAgent = "Node-Cod";
+
+const infiniteWarfare = "iw";
+const worldWar2 = "wwii";
+const blackops3 = "bo3";
+
+exports.IwWeekly = function (gamertag, platform) {
+    return new Promise((resolve, reject) => {
+        var urlInput = defaultBaseURL + util.format("crm/cod/v2/title/%s/platform/%s/gamer/%s/summary/", infiniteWarfare, platform, gamertag);
+        var options = {
+            url: urlInput,
+            headers: {
+                "content-type": "application/json",
+                "User-Agent": userAgent
+            }
+        };
+        request(options, (e, r, b) => {
+            if(e) reject(e);
+            resolve(JSON.parse(b).data);
+        });
+    });
+};
+
+exports.IWStats = function (gamertag, platform) {
+    return new Promise((resolve, reject) => {
+        var urlInput = defaultBaseURL + util.format("crm/cod/v2/title/%s/platform/%s/gamer/%s/profile/", infiniteWarfare, platform, gamertag);
+        var options = {
+            url: urlInput,
+            headers: {
+                "content-type": "application/json",
+                "User-Agent": userAgent
+            }
+        };
+        request(options, (e, r, b) => {
+            if(e) reject(e);
+            resolve(JSON.parse(b).data);
+        });
+    });
+};
+
+exports.WWIIWeekly = function (gamertag, platform) {
+    return new Promise((resolve, reject) => {
+        var urlInput = defaultBaseURL + util.format("crm/cod/v2/title/%s/platform/%s/gamer/%s/summary/", worldWar2, platform, gamertag);
+        var options = {
+            url: urlInput,
+            headers: {
+                "content-type": "application/json",
+                "User-Agent": userAgent
+            }
+        };
+        request(options, (e, r, b) => {
+            if(e) reject(e);
+            resolve(JSON.parse(b).data);
+        });
+    });
+};
+
+exports.WWIIStats = function (gamertag, platform) {
+    return new Promise((resolve, reject) => {
+        var urlInput = defaultBaseURL + util.format("crm/cod/v2/title/%s/platform/%s/gamer/%s/profile/", worldWar2, platform, gamertag);
+        var options = {
+            url: urlInput,
+            headers: {
+                "content-type": "application/json",
+                "User-Agent": userAgent
+            }
+        };
+        request(options, (e, r, b) => {
+            if(e) reject(e);
+            resolve(JSON.parse(b).data);
+        });
+    });
+};
+
+exports.BO3Stats = function (gamertag, platform) {
+    return new Promise((resolve, reject) => {
+        var urlInput = defaultBaseURL + util.format("crm/cod/v2/title/%s/platform/%s/gamer/%s/profile/", blackops3, platform, gamertag);
+        var options = {
+            url: urlInput,
+            headers: {
+                "content-type": "application/json",
+                "User-Agent": userAgent
+            }
+        };
+        request(options, (e, r, b) => {
+            if(e) reject(e);
+            resolve(JSON.parse(b).data);
+        });
+    });
+};
